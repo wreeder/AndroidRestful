afebd275d1f8eceae177b8c7cae1caa9292e07a1
William Reeder <wreeder1@gmail.com>
2015-12-13 20:44:49 -0500

    Commit All Files

---
+++ app/src/main/java/com/skyenet/rest/Greeting.java
@@ -0,0 +1,20 @@
+package com.skyenet.rest;
+
+/**
+ * Created by William Reeder on 12/13/2015.
+ */
+
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+}
